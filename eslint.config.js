29c26d8978abad484eafae78fbc616ccc080fc4f
Kavin <kavin.ned@gmail.com>
2024-12-06 19:19:32 +0800

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import { getMenu } from "./src/services/apiRestaurant";
 
 export default [
 	{ ignores: ["dist"] },
@@ -29,6 +28,7 @@ export default [
 			...react.configs.recommended.rules,
 			...react.configs["jsx-runtime"].rules,
 			...reactHooks.configs.recommended.rules,
+			"react/prop-types": 0,
 			"react/jsx-no-target-blank": "off",
 			// "react-refresh/only-export-components": [
 			// 	"warn",
