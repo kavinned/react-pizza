2306aaa0a14008b1876017e4b53a732cc6b6c944
Kavin <kavin.ned@gmail.com>
2024-12-08 00:53:21 +0800

    styling

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,10 +6,7 @@ export default function Header() {
     return (
         <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6 font-primary font-semibold">
             <Link to="/" className="tracking-widest">
-                <span className="bg-yellow-700 px-1 py-1 rounded-full mx-1">
-                    🍕
-                </span>
-                React Pizza{" "}
+                React Pizza <span className="header-emoji">🍕</span>
             </Link>
             <div className="md:absolute md:left-1/2 md:-translate-x-1/2">
                 <SearchOrder />
